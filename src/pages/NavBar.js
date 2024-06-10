81a346ea3df78184f222c8e774a6c58cfa622205
WEL-ADNET\C_ZHAO <Chenyu_Zhao-p@wellnet.co.jp>
2024-06-10 17:33:05 +0900

    React 画面遷移可能

---
+++ src/pages/NavBar.js
@@ -0,0 +1,65 @@
+import Inform from './Inform';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Breadcrumbs, Card, Grid } from '@aws-amplify/ui-react';
+
+
+const NavBar = () => {
+  return (
+    
+    <Grid
+      columnGap="0.5rem"
+      rowGap="0.5rem"
+      templateColumns="1fr 1fr 1fr"
+      templateRows="1fr 3fr 1fr"
+
+      style={{
+        width: '100vw', 
+        height: '900px',
+        overflow: 'hidden' 
+        
+      }}
+    >
+      <Card
+        columnStart="1"
+        columnEnd="-1"
+      >
+        <Breadcrumbs.Container>
+
+
+
+          <Breadcrumbs.Item>
+            <Breadcrumbs.Link href='/inform'  >
+              お知らせ
+            </Breadcrumbs.Link>
+            <Breadcrumbs.Separator />
+          </Breadcrumbs.Item>
+
+
+
+        </Breadcrumbs.Container>
+      </Card>
+
+
+      <Card
+        columnStart="1"
+        columnEnd="-1"
+      >
+        <Router>
+          <Routes>
+            <Route path="/inform" element={<Inform />} />
+          </Routes>
+        </Router>
+      </Card>
+
+      <Card
+        columnStart="1"
+        columnEnd="-1"
+      >
+        Footer
+      </Card>
+    </Grid>
+
+
+  );
+}
+export default NavBar;
